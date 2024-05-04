ef7047ad903e7445719071397ca1c7002ae23301
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-05-04 14:15:00 +0530

    name correction and comments

---
+++ src/models/subscription.model.js
@@ -0,0 +1,37 @@
+import mongoose, {Schema} from "mongoose";
+
+/**
+ * @description Subscription schema for the application
+ * @type {Schema}
+ */
+const subscriptionSchema = new Schema({
+  /**
+   * @description Subscribing user
+   * @type {ObjectId}
+   * @ref User
+   */
+  subscriber: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  /**
+   * @description Channel owner
+   * @type {ObjectId}
+   * @ref User
+   */
+  channel: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  /**
+   * @description Timestamps for creation and update
+   * @type {Date}
+   */
+  timestamps: true,
+});
+
+/**
+ * @description Subscription model for the application
+ * @type {Model}
+ */
+export const Subscription = mongoose.model('Subscription', subscriptionSchema);
